69861c51834572f13ac9ae260ef7473912b90248
BinbinTang <BinbinTang@r020149.bs.swh.mhn.de>
2014-04-11 22:11:15 +0200

    add the code file

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+
+
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
