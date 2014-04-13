ea59d99466ccca983fc26a65aed1a98af86c9f2b
cyk235 <ykchowab@stu.ust.hk>
2014-04-14 06:04:46 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -8,10 +8,13 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <algorithm>
+#include <queue>
 
 void externalSort (int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
-	if (fdInput<)
-
+	read(fdInput,,memSize);
+	vector<uint64_t> SortRuns;
+	open(fdInput,,memSize);
 }
 
 
@@ -20,7 +23,7 @@ int main(int argc, char* argv[]) {
 
 	int fdInput =open(argv[1],O_RDONLY);
 	int fdOutput=open(argv[2],O_CREAT|O_TRUNC|O_WRONLY);
-	int memSize =argv[3];
+	int memSize =argv[3]*1024;
 	externalSort (fdInput,64,fdOutput,memSize);
 	return 0;
 }
