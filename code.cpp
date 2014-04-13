8b793ef13df9a95deb8f50a94d86f3deafc745ef
cyk235 <ykchowab@stu.ust.hk>
2014-04-14 03:17:58 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -10,7 +10,7 @@
 #include <unistd.h>
 
 void externalSort (int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
-
+	if (fdInput<)
 
 }
 
@@ -19,7 +19,7 @@ void externalSort (int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
 int main(int argc, char* argv[]) {
 
 	int fdInput =open(argv[1],O_RDONLY);
-	int fdOutput=open(argv[2],O_WRONLY);
+	int fdOutput=open(argv[2],O_CREAT|O_TRUNC|O_WRONLY);
 	int memSize =argv[3];
 	externalSort (fdInput,64,fdOutput,memSize);
 	return 0;
