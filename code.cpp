45c0fbbd3807bcc3d86424e8d75b87637182a664
cyk235 <ykchowab@stu.ust.hk>
2014-04-15 06:31:07 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -15,14 +15,18 @@ void externalSort (int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
 	vector< vector<uint64_t> > SortRuns;
 	vector<uint64_t> IndivRun;
 	uint64_t x;
-	while (!(read(fdInput, &x, sizeof(uint64_t)) < 0) // divide and sort individual runs
+	uint64_t cntsize=0;
+	while (!(read(fdInput, &x, sizeof(uint64_t)) < 0)) // divide and sort individual runs
 	{
 		IndivRun.push_back(x);
 		size++;
-		if (size==IndivRun.size())
+		cntsize++;
+		if (cntsize==IndivRun.size())
 		{
 			std::sort (IndivRun.begin(), IndivRun.end());
 			SortRuns.push_back(IndivRun);
+			IndivRun.clear();
+			cntsize=0;
 		}
 		
 	}
@@ -30,6 +34,33 @@ void externalSort (int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
 	{
 		std::sort (IndivRun.begin(), IndivRun.end());
 		SortRuns.push_back(IndivRun);
+		IndivRun.clear();
+	}
+	
+	vector<uint64_t> Result; // merge 
+	uint64_t smallest=SortRuns[0].front();
+	int position=0;
+	while (SortRuns.size()>0) {  // find the smallest among the first elements of the runs
+		for (int i=0; i<SortRuns.size(); i++) {
+			if (SortRuns[i].front()< smallest) {
+				smallest=SortRuns[i].front();
+				position=i;
+			}
+		}
+		Result.push_back(SortRuns[position].front());
+		SortRuns[position].erase(SortRuns[position].begin());
+		for (i =0; i < SortRuns.size();) { // remove runs with no element
+			if (SortRuns[i].size()<=0)
+			{
+				SortRun.erase(SortRun.begin()+i);
+			}
+			else
+			{
+        			i++;
+			}
+		
+		}
+		
 	}
 	
 	for (int i=0; i<Result.size(); i++) {   // write the result
@@ -44,7 +75,7 @@ int main(int argc, char* argv[]) {
 
 	int fdInput =open(argv[1],O_RDONLY);
 	int fdOutput=open(argv[2],O_CREAT|O_TRUNC|O_WRONLY);
-	int memSize =argv[3]*1048576;
+	uint64_t memSize =(uint64_t)(atoi(argv[3]))*1048576;
 	externalSort (fdInput,0,fdOutput,memSize);
 	return 0;
 }
